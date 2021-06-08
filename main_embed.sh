ed27a874ef961c924ca2f84ae729cccb8d42e3cd
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-06-08 16:29:58 -0700

    v1.6.0 -- cloned from DGAPN

---
+++ main_embed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+eval "$(conda shell.bash hook)"
+conda activate my-rdkit-new
+
+DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+PYARGS=""
+PYARGS="$PYARGS --name predict_dock_default"
+PYARGS="$PYARGS --data_path $DATA/src/dataset/NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.csv"
+PYARGS="$PYARGS --artifact_path $DATA/artifact/dgapn"
+# PYARGS="$PYARGS --use_3d"
+
+python src/main_embed.py $PYARGS
